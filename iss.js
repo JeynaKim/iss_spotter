26b18b510503f79fa84cdaa00e27d00943b06782
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-13 03:05:44 +0000

    ISS Spotter II completed

---
+++ iss.js
@@ -0,0 +1,47 @@
+const request = require("request");
+
+const fetchMyIP = function (callback) {
+  // use request to fetch IP address from JSON API
+
+  request("https://api.ipify.org?format=json", (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    console.log(ip);
+    callback(err, ip);
+  });
+};
+
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchCoordsByIP };
